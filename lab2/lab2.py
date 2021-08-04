370314e2e57568cfd5116f7eb3bfc4a0af8c5957
Eduardo Penedo <eduardorodriguespenedo@gmail.com>
2021-08-04 12:00:49 -0300

    Função idade com parâmetro data podendo ser passado com qualquer data, anteriormente estava calculando a idade apenas com a data do dia

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -3,12 +3,14 @@
 
 
 # AULA 2
-from datetime import * 
+from datetime import *
+
+
 class Aluno:
-    def __init__(self, nome, DRE, matricula = "ativa"):
+    def __init__(self, nome, DRE, matricula="ativa"):
         """Cria um objeto da classe Aluno com atributos nome, DRE, matricula"""
         self.nome = nome
-        self.DRE = DRE       
+        self.DRE = DRE
         self.matricula = matricula
 
     def alterarMatricula(self, matricula):
@@ -18,7 +20,7 @@ class Aluno:
         else:
             self.matricula = matricula
             print("A matrícula foi alterada para " + matricula)
-            
+
     def dados(self):
         """Retorna uma descrição de um objeto da classe"""
         return "{}\t{}\tmatricula {}".format(self.nome, str(self.DRE), self.matricula)
@@ -27,16 +29,17 @@ class Aluno:
         """Retorna uma descrição de um objeto da classe"""
         return "{}\t{}\tmatricula {}".format(self.nome, str(self.DRE), self.matricula)
 
-        
-#=======================================
+
+# =======================================
 
 class Disciplina:
     """Classe representa o conceito de uma disciplina na UFRJ"""
-    def __init__(self, nome, vagas = 0):
+
+    def __init__(self, nome, vagas=0):
         """Cria um objeto da classe com atributos nome, vagas, alunos"""
         self.nome = nome
         self.vagas = vagas
-        self.alunos = [] # self.alunos é um atributo global criado automaticamente
+        self.alunos = []  # self.alunos é um atributo global criado automaticamente
 
     def __add__(self, other):
         """Junta duas disciplinas se o nome delas for idêntico"""
@@ -44,7 +47,7 @@ class Disciplina:
             return Disciplina(self.nome, self.vagas + other.vagas)
         else:
             print("Não foi possível juntar as turmas")
-    
+
     def __str__(self):
         """ Retorna string com dados da disciplina e de alunos inscritos nela"""
         if len(self.alunos) == 0:
@@ -52,10 +55,10 @@ class Disciplina:
         else:
             alunos_dados = ""
             for i in range(len(self.alunos)):
-                alunos_dados += "{} {} matrícula {}\n".format(self.alunos[i].nome, self.alunos[i].DRE, self.alunos[i].matricula)
+                alunos_dados += "{} {} matrícula {}\n".format(
+                    self.alunos[i].nome, self.alunos[i].DRE, self.alunos[i].matricula)
 
-            return "{}, alunos inscritos:\n{}{}".format(self.nome, alunos_dados, self.consultarVagas(),0 )
-
+            return "{}, alunos inscritos:\n{}{}".format(self.nome, alunos_dados, self.consultarVagas(), 0)
 
     def inscreverAluno(self, aluno):
         """Inscreve um objeto da classe Aluno na disciplina se tiver
@@ -66,39 +69,48 @@ class Disciplina:
             print("aluno {} já está inscrito na disciplina".format(aluno.nome))
         else:
             print("Vagas esgotadas")
-    
+
     def consultarVagas(self):
         """ Retorna quantidade de vagas totais e vagas livres ( vagas totais - vagas ocupadas por objetos da classe Aluno )"""
         vagasDisponiveis = self.vagas - len(self.alunos)
-        return "Vagas totais:{} Vagas livres:{}".format(self.vagas,vagasDisponiveis)
+        return "Vagas totais:{} Vagas livres:{}".format(self.vagas, vagasDisponiveis)
 
-#=======================================
+# =======================================
+
 
 class Pessoa:
     def __init__(self, nome, dataNascimento, nomeDeMae, nomeDePai):
-       """ Inicia um objeto do tipo Pessoa a partir dos parâmetros nome, dataNascimento, nomeDeMae, nomeDePai"""
-       self.nome = nome
-       self.dataNascimento = dataNascimento
-       self.nomeDeMae =nomeDeMae
-       self.nomeDePai = nomeDePai
-    
+        """ Inicia um objeto do tipo Pessoa a partir dos parâmetros nome, dataNascimento, nomeDeMae, nomeDePai"""
+        self.nome = nome
+        self.dataNascimento = dataNascimento
+        self.nomeDeMae = nomeDeMae
+        self.nomeDePai = nomeDePai
+
     def __str__(self):
         """Retorna string com dados de um objeto Pessoa"""
         return "Nome: {} Idade: {} Mãe: {} Pai: {}".format(self.nome, self.idade(), self.nomeDeMae, self.nomeDePai)
 
-    def idade(self, data = date.today().strftime("%d/%m/%Y")):
+    def idade(self, data=date.today().strftime("%d/%m/%Y")):
         """ Retorna a quantidade de anos entre a data atual e a data de nascimento """
-        dia_nasc = int(self.dataNascimento[:2])
-        mes_nasc = int(self.dataNascimento[3:5])
-        ano_nasc = int(self.dataNascimento[6:])
-        
+        data_nasc = date(int(self.dataNascimento[6:]), int(
+            self.dataNascimento[3:5]), int(self.dataNascimento[:2]))
 
-        if date.today().month < mes_nasc:
-            return (date.today().year - ano_nasc) -1
-        elif date.today().month == mes_nasc:
-            if date.today().day < dia_nasc:
-                return date.today().year - ano_nasc -1
+        data_final = date(int(data[6:]), int(data[3:5]), int(data[:2]))
+
+        if data_final.month < data_nasc.month:
+            return (data_final.year - data_nasc.year) - 1
+        elif data_final.month == data_nasc.month:
+            if data_final.day < data_nasc.day:
+                return data_final.year - data_nasc.year - 1
             else:
-                return date.today().year - ano_nasc
+                return data_final.year - data_nasc.year
         else:
-            return date.today().year - ano_nasc
+            return data_final.year - data_nasc.year
+
+
+if __name__ == "__main__":
+    leo = Pessoa('Leo', '13/09/2001', 'Elaine', 'Julio')
+    print(leo)
+    print(leo.dataNascimento)
+    print(str(leo.idade()))
+    print(str(leo.idade('22/10/2025')))
